5f4017c022d189cf6a729f64573515857d469de0
Ricky Chen <rc3297@drexel.edu>
2025-03-04 13:21:20 -0500

    integrate: finish numerical integration implementation

--- numerical_integration/source_code/trap.cu
+++ numerical_integration/source_code/trap.cu
@@ -1,14 +1,14 @@
 /* Reference code implementing numerical integration.
  *
- * Build and execute as follows: 
-        make clean && make 
-        ./trap a b n 
+ * Build and execute as follows:
+        make clean && make
+        ./trap a b n
 
  * Author: Naga Kandasamy
  * Date modified: February 28, 2025
 
- * Student name(s): FIXME
- * Date modified: FIXME
+ * Student name(s): Ricky Chen, Keith Trungcao
+ * Date modified: 3/2/25
 */
 
 #include <stdlib.h>
@@ -24,7 +24,7 @@
 double compute_on_device(float, float, int, float);
 extern "C" double compute_gold(float, float, int, float);
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     if (argc < 4) {
         fprintf(stderr, "Usage: %s a b n\n", argv[0]);
@@ -36,24 +36,41 @@ int main(int argc, char **argv)
 
     int a = atoi(argv[1]); /* Left limit */
     int b = atoi(argv[2]); /* Right limit */
-    int n = atoi(argv[3]); /* Number of trapezoids */ 
+    int n = atoi(argv[3]); /* Number of trapezoids */
 
-    float h = (b-a)/(float)n; // Height of each trapezoid  
-	printf("Number of trapezoids = %d\n", n);
+    float h = (b-a)/(float)n; // Height of each trapezoid
+    printf("Number of trapezoids = %d\n", n);
     printf("Height of each trapezoid = %f \n", h);
 
-	double reference = compute_gold(a, b, n, h);
+    double reference = compute_gold(a, b, n, h);
     printf("Reference solution computed on the CPU = %f \n", reference);
 
-	/* Write this function to complete the trapezoidal on the GPU. */
-	double gpu_result = compute_on_device(a, b, n, h);
-	printf("Solution computed on the GPU = %f \n", gpu_result);
-} 
+    /* Write this function to complete the trapezoidal on the GPU. */
+    double gpu_result = compute_on_device(a, b, n, h);
+    printf("Solution computed on the GPU = %f \n", gpu_result);
+}
 
 /* Complete this function to perform the trapezoidal rule on the GPU. */
 double compute_on_device(float a, float b, int n, float h)
 {
-    return 0.0;
+    double *d_result;
+    double h_result = 0.0;
+
+    cudaMalloc((void**)&d_result, sizeof(double));
+
+    cudaMemcpy(d_result, &h_result, sizeof(double), cudaMemcpyHostToDevice);
+
+    int threads_per_block = 1024;
+    int num_blocks = (n + threads_per_block - 1) / threads_per_block;
+
+    trap_kernel<<<num_blocks, threads_per_block>>>(a, b, h, n, d_result);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(&h_result, d_result, sizeof(double), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_result);
+
+    return h_result * h;
 }
 
 
--- numerical_integration/source_code/trap_kernel.cu
+++ numerical_integration/source_code/trap_kernel.cu
@@ -1,14 +1,49 @@
 /* GPU kernel to estimate integral of the provided function using the trapezoidal rule. */
 
-/* Device function which implements the function. Device functions can be called from within other __device__ functions or __global__ functions (the kernel), but cannot be called from the host. */ 
+/* Device function which implements the function. Device functions can be called from within other __device__ functions or __global__ functions (the kernel), but cannot be called from the host. */
 
-__device__ float fd(float x) 
+#define THREAD_BLOCK_SIZE 1024
+
+__device__ float fd(float x)
 {
      return sqrtf((1 + x*x)/(1 + x*x*x*x));
 }
 
 /* Kernel function */
-__global__ void trap_kernel(float a, float b, int n, float h) 
+__global__ void trap_kernel(float a, float b, float h, int n, double *result)
 {
-     
+    __shared__ double partial_sum[THREAD_BLOCK_SIZE];
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = blockDim.x * gridDim.x;
+    double sum = 0.0;
+    unsigned int i = tid;
+
+    if (i > 0) {
+        while (i < n) {
+            sum += fd(a + i * h);
+            i += stride;
+        }
+    }
+
+    partial_sum[threadIdx.x] = sum;
+    __syncthreads();
+
+    i = blockDim.x / 2;
+    while (i != 0) {
+        if (threadIdx.x < i) {
+            partial_sum[threadIdx.x] += partial_sum[threadIdx.x + i];
+        }
+        __syncthreads();
+
+        i /= 2;
+    }
+
+    if (threadIdx.x == 0 && blockIdx.x == 0) {
+        atomicAdd(result, (fd(a) + fd(b)) / 2.0);
+    }
+
+    if (threadIdx.x == 0) {
+        atomicAdd(result, partial_sum[0]);
+    }
 }
+
